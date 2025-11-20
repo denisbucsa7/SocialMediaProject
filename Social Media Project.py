638e929427192fe3cddc2a637ce11bf0fa0735f1
Denis Bucsa <denisbucsa7@gmail.com>
2025-11-20 12:37:39 +0000

    Add files via upload
    Added a few of the initial classes like User, Social media app and post as well as starting the queue class. I have also got the node and hash table class since I'm going to be storing the user information in a hash table.

---
+++ Social Media Project.py
@@ -0,0 +1,108 @@
+#Social Media Project 
+class Node:
+    def __init__(self,key,value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashTable:
+    def __init__(self,size=50):
+        self.size = size
+        self.table = None * size
+
+    def hash(self,key):
+        return hash(key) % self.size
+    
+    def insert(self,key, value):
+        index = self._hash(key)
+        head = self.table[index]
+
+        current = head
+        while current:
+            if current.key == key:
+                current.value = value
+                return
+            current = current.next
+
+        new_node = Node(key,value)
+        new_node.next = head
+        self.table[index] = new_node
+
+    def get(self,key):
+        index = self._hash(key)
+        current = self.table[index]
+
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
+    
+    def delete(self,key):
+        index = self._hash(key)
+        current = self.table[index]
+        previous = None
+
+        while current:
+            if current.key == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.table[index] = current.next
+                return True
+            previous = current
+            current = current.next
+
+        return False
+    
+#System Clases:
+class User:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.posts = []
+        self.friends = []
+
+    def add_friend(self,username):
+        if username not in self.friends:
+            self.friends.append(username)
+
+    def add_post(self, content):
+        self.posts.append(content)
+
+class Post:
+    def __init__(self,author, content):
+        self.author = author
+        self.content = content
+
+class SocialMediaApp:
+    def __init__(self):
+        self.users = HashTable()
+        self.posts = []
+
+    def register(self, username, password):
+        if self.users.get(username) is not None:
+            return "Username already taken"
+        
+        new_user = User(username.password)
+        self.users.insert(username, new_user)
+        return "User registered"
+    
+    def login(self,username,password):
+        user = self.users.get(username)
+        if user and user.password == password:
+            return "Log in succesful"
+        return "Invalid Information"
+    
+    def create_post(self,username,content):
+        user = self.users.get(username)
+        if user:
+            new_post = Post(username,content)
+            user.add_post(new_post)
+            self.posts.append(new_post)
+            return "Post uploaded"
+        return "User not found"
+
+class Queue:
+    def __init__(self):
+        self
